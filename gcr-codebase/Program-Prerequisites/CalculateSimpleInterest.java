33afa981229ccc08f4dc510b1ea27b5e97b6f7f1
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-16 16:44:44 +0530

    [Arvind] : Added the program of calculate the simple interest

--- gcr-codebase/Program-Prerequisites/CalculateSimpleInterest.java
+++ gcr-codebase/Program-Prerequisites/CalculateSimpleInterest.java
@@ -1,8 +1,20 @@
-import java.util.*;
+import java.util.Scanner;
 public class CalculateSimpleInterest{
 	public static void main (String args[]){
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Hello");
+		System.out.println("Enter the principle: ");
+		int principle = sc.nextInt();
+		
+		System.out.println("Enter the rate of interest : ");
+		double rate = sc.nextDouble();
+		
+		System.out.println("Enter the time for interest: ");
+		int time = sc.nextInt();
+		
+		double simpleInterest = (principle * rate * time) / 100;
+		
+		System.out.println(simpleInterest);
+
 	}
 }
