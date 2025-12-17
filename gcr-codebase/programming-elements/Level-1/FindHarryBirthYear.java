dfea98de1b8985adb1f937383daa575b7567cddb
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-17 11:43:34 +0530

    [Arvind] : Leve1-1 -> Added the program file of finding harray birth year

---
+++ gcr-codebase/programming-elements/Level-1/FindHarryBirthYear.java
@@ -0,0 +1,9 @@
+public class FindHarryBirthYear{
+	public static void main (String args[]){
+		int harryBirth = 2000;
+		int currentYear = 2024;
+		
+		System.out.println("Harry's age in 2024 is: "+(currentYear-harryBirth));
+
+	}
+}
