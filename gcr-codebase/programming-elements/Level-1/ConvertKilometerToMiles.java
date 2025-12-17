99bf54976366fbf30cee9adf4e3dcaf6b053a35f
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-17 12:16:08 +0530

    [Arvind] : Added the program file of Convert Kilometer into Miles

---
+++ gcr-codebase/programming-elements/Level-1/ConvertKilometerToMiles.java
@@ -0,0 +1,8 @@
+public class ConvertKilometerToMiles{
+	public static void main (String args[]){
+		double distanceKilometer = 10.8;
+		
+		double convertMiles = distanceKilometer/1.609;
+		System.out.println("The distance " + distanceKilometer + " km in miles is : "+ (convertMiles));
+	}
+}
