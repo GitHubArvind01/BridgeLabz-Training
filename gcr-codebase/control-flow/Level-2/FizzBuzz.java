9c0984c6362a4c24fae5d73c9ea71407d071e614
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-18 16:14:46 +0530

    [Arvind : Level-2 -> Added the program file of FizzBuzz

---
+++ gcr-codebase/control-flow/Level-2/FizzBuzz.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+//Create bonusAmount Class to compute the salary bouns amount
+public class FizzBuzz{
+	public static void main (String args[]){
+		// Creating Scanner object to take input from user
+        Scanner input = new Scanner(System.in);
+		
+		// Taking number as input
+		System.out.println("Enter the positive interger: ");
+		
+		if(input.hasNextInt()){
+			int number = input.nextInt();
+			if(number>0){
+				for(int i=1;i<=number;i++){
+					if(i%15==0){
+						System.out.println("FizzBuzz");
+					}
+					else if(i%3==0){
+						System.out.println("Fizz");
+					}
+					else if(i%5==0){
+						System.out.println("Buzz");
+					}else{
+						System.out.println(i);
+					}
+				}
+			}else{
+				System.out.println("Error: Enter postive number!");
+			}
+		}else{
+			System.out.println("Error: Enter vaild number as interger!");
+		}
+	}
+}
