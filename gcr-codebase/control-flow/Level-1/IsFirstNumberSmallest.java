eb95f9a70dc11f5a61211bf961c5b19c0c935d77
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-18 12:57:26 +0530

    [Arvind : Level-1 -> Added the program file of is the first number smallest

---
+++ gcr-codebase/control-flow/Level-1/IsFirstNumberSmallest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+//Create IsFirstNumberSmallest Class to Print the yes or no
+public class IsFirstNumberSmallest{
+	public static void main (String args[]){
+		// Creating Scanner object to take input from user
+        Scanner input = new Scanner(System.in);
+		
+		// Taking three number as input
+		System.out.println("Enter the first number: ");
+		int number1 = input.nextInt();
+		
+		System.out.println("Enter the second number: ");
+		int number2 = input.nextInt();
+		
+		System.out.println("Enter the third number: ");
+		int number3 = input.nextInt();
+		
+		
+		// Displaying the output
+
+		if(number1<=number2 && number1<=number3){
+			System.out.println("Is the first number the smallest? Yes");
+		}else{
+			System.out.println("Is the first number the smallest? No");
+		}
+	}
+}
