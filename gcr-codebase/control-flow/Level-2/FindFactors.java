4dc79070a7e3567ef8e53fe93c71c018203c75b8
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-18 16:46:05 +0530

    [Arvind : Level-2 -> Added the program file of finding factors

---
+++ gcr-codebase/control-flow/Level-2/FindFactors.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+//Create FindFactors Class to compute the factor from 1 to number
+public class FindFactors{
+	public static void main (String args[]){
+		// Creating Scanner object to take input from user
+        Scanner input = new Scanner(System.in);
+		
+		// Taking number as input
+		System.out.println("Enter the Postive number: ");
+		while(true){
+			int number = input.nextInt();
+			//checking number is positive or not Displaying the ouput
+			if(number>0){
+				for(int i=1;i<number;i++){
+					if(number%i==0){
+						System.out.println(i);
+					}
+				}
+				break;
+			}
+			System.out.println("Again! Enter the Postive number: ");
+		}		
+	}
+}
