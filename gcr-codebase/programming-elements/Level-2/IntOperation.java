eef3afa2088199c4dc60e4f54847ff5ba9327596
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-18 11:51:34 +0530

    [Arvind] : Added the program of Arithmetic Operation

--- gcr-codebase/programming-elements/Level-2/IntOperation.java
+++ gcr-codebase/programming-elements/Level-2/IntOperation.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
+//Create IntOperation Class to compute the Distance and Travel Time
 public class IntOperation{
 	public static void main (String args[]){
-		System.out.println("Hello");
+		// Creating Scanner object to take input from user
+        Scanner input = new Scanner(System.in);
+		
+		// Taking three number as input
+		System.out.println("Enter the first number: ");
+		int a = input.nextInt();
+		
+		System.out.println("Enter the second number: ");
+		int b = input.nextInt();
+		
+		System.out.println("Enter the third number: ");
+		int c = input.nextInt();
+		
+		// Displaying the output
+		System.out.println("The results of Int Operations are "+ (a + b *c ) +", " + (a * b + c) +", "+ (c + a / b) +", and "+(a % b + c));
 	}
 }
