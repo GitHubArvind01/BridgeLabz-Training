2b263b35cf206c69843c69e9d4d4501ff708adc8
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-19 15:29:52 +0530

    [Arvind] : Level-1 -> Added the program file of checking number

--- gcr-codebase/arrays/Level-1/CheckNumber.java
+++ gcr-codebase/arrays/Level-1/CheckNumber.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CheckNumber{
 	public static void main (String args[]){
 		
-		// Creating Scanner object to take input from user
+		// Creating Scanner object to take input from user 
         Scanner sc = new Scanner(System.in);
 		
 		//creating the array age of size 5 that store number
@@ -16,7 +16,7 @@ public class CheckNumber{
 		}		
 
 		//Displaying the ouptut
-		for(int i=0;i<10;i++){
+		for(int i=0;i<5;i++){
 			if(number[i]<0){
 				System.out.println("This is negative number!");
 			}else if(number[i]==0){
@@ -28,6 +28,11 @@ public class CheckNumber{
 					System.out.println(number[i] + " is odd.");
 				}
 			}
+		}
+		if(number[0]!=number[5-1]){
+			System.out.println("First element " + number[0] + " And last element " + number[5-1] + "Equal.");
+		}else{
+			System.out.println("First element " + number[0] + " And last element " + number[5-1] + "Not Equal.");
 		}
 	}
 }
