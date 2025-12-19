93e3fea603657327d6b6d4c0070029fce151af5e
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-19 15:30:28 +0530

    [Arvind] : Level-1 -> Added the program file of All number sum until they enter zero or negative

---
+++ gcr-codebase/arrays/Level-1/AllNumberSum.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+//Create AllNumberSum Class to compute the sum of totoal number until the user enterd zero or negative
+public class AllNumberSum{
+	public static void main (String args[]){
+		
+		// Creating Scanner object to take input from user
+        Scanner sc = new Scanner(System.in);
+		
+		//creating the array age of size 10 stored the multiplication of number 
+		int number[] = new int[10];
+		
+		//Taking input and computing the number sum if they are not zero and negative
+		int numberSum = 0;
+		int i=0;
+		while(true){
+			System.out.println("Enter the number : ");
+			number[i] = sc.nextInt();
+			if(number[i]==0 || number[i]<0){
+				break;
+			}else{
+				numberSum+=number[i];
+			}
+		}
+		
+
+		//Displaying the ouptut
+		System.out.println("The sum of total number: "+ numberSum);
+	}
+}
