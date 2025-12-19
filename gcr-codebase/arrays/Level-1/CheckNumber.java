92b8e5e3040bc10ad03d6942682ea5d4acd52ea8
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-19 14:58:03 +0530

    [Arvind] : Level-3 -> Added the program file of number check

---
+++ gcr-codebase/arrays/Level-1/CheckNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+//Create CheckNumber Class to compute the wheather number is positive, negative, and zero
+public class CheckNumber{
+	public static void main (String args[]){
+		
+		// Creating Scanner object to take input from user
+        Scanner sc = new Scanner(System.in);
+		
+		//creating the array age of size 5 that store number
+		int number[] = new int[5];
+		
+		//Taking age as input from user
+		for(int i=0;i<5;i++){
+			System.out.println("Enter the number"+(i+1) + " : ");
+			number[i] = sc.nextInt();
+		}		
+
+		//Displaying the ouptut
+		for(int i=0;i<10;i++){
+			if(number[i]<0){
+				System.out.println("This is negative number!");
+			}else if(number[i]==0){
+				System.out.println("This is zero");
+			}else{
+				if(number[i]%2==0){
+					System.out.println(number[i] + " is even.");
+				}else{
+					System.out.println(number[i] + " is odd.");
+				}
+			}
+		}
+	}
+}
