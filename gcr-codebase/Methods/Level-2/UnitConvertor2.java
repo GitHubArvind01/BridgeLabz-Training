518e12e92fae58db9c5ff5ea826955441d804406
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-21 22:52:53 +0530

    [Arvind] : Level-2 -> Added the program file of unit converter part2

---
+++ gcr-codebase/Methods/Level-2/UnitConvertor2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+//Create UnitConvertor2 Class to convert the unit into another
+public class UnitConvertor2{
+	public static void main (String args[]){
+		
+		//This is convert yard to feets
+		double yard = 20;
+		System.out.println("20 yard convert feet : " + convertYardToFeets(yard));
+		
+		//This is convert feet to yard
+		double feet = 20;
+		System.out.println("20 miles convert km : " + convertFeetToYard(feet)); 
+		
+		//This is convert meters to feets
+		double meters = 20;
+		System.out.println("20 meters convert feets : " + convertMetersToInches(meters)); 
+		
+		//This is convert inches to meters
+		double inches = 20;
+		System.out.println("20 feets convert meters : " + convertInchesToMeters(inches)); 
+		
+		//This is convert inches to centimeters
+		System.out.println("20 feets convert meters : " + convertInchesTocentimeters(inches)); 
+	}
+	public static double convertYardToFeets(double yard){
+		double yards2feet = 3;;
+		return yard*yards2feet;
+	}
+	
+	public static double convertFeetToYard(double feet){
+		double feet2yards = 0.333333;
+		return feet*feet2yards;
+	}
+	
+	public static double convertMetersToInches(double meters){
+		double meters2inches = 39.3701;
+		return meters*meters2inches;
+	}
+	
+	public static double convertInchesToMeters(double inches){
+		double inches2meters = 0.0254;
+		return inches*inches2meters;
+	}
+	
+	public static double convertInchesTocentimeters(double inches){
+		double inches2cm = 2.54;
+		return inches*inches2cm;
+	}
+}
