8eda156484e89d99145df5599befeace549956df
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-24 12:40:49 +0530

    [ARVIND] : leetcode-> Added the program file of is valid paranthess

---
+++ leet-code-codebase/strings/isValid.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<n;i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else{
+                if(st.isEmpty()){
+                    return false;
+                }else{
+                    char ct = st.peek();
+                    if((ct=='(' && c==')') || (ct=='{' && c=='}') || (ct=='[' && c==']')){
+                        st.pop();
+                    }else{
+                        return false;
+                    } y
+                }
+            }
+        }
+        if(st.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
