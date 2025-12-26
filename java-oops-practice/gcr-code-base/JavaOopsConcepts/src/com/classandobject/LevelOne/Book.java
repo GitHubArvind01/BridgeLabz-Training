7f8b6c92c8854c350d8dc9d8c44eecdea814cc75
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-26 17:12:53 +0530

    [ARVIND] : Level-1 -> Added the program file of Book display details

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/classandobject/LevelOne/Book.java
@@ -0,0 +1,14 @@
+package com.classandobject.LevelOne;
+
+public class Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HandBook book1= new HandBook("2States", "Chetan Bhagat", 500);
+		HandBook book2=new HandBook("Wings Of Fire","Abdul kalam.A.P.J",  500);
+		
+		book1.displayBookDetails();
+		book2.displayBookDetails();
+	}
+
+}
---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/classandobject/LevelOne/HandBook.java
@@ -0,0 +1,23 @@
+package com.classandobject.LevelOne;
+
+public class HandBook {
+	String bookTitle;
+	String bookAuthor;
+	double price;
+	
+	//constructor
+	HandBook(String bookTitle,String bookAuthor,double price){
+		this.bookTitle=bookTitle;
+		this.bookAuthor=bookAuthor;
+		this.price=price;
+	}
+	
+	//creating the method to display the output
+	public void displayBookDetails() {
+		System.out.println("------------------------------");
+		System.out.println("Title of the book: "+bookTitle);
+		System.out.println("Author of the book: "+bookAuthor);
+		System.out.println("Price of the book: "+price);
+	}
+
+}
