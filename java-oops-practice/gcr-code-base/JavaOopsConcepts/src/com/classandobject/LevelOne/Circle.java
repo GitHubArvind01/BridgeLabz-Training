d958008f7affec48903bbb33227a7176634bf843
GitHubArvind01 <kumararvind21390@gmail.com>
2025-12-26 16:24:49 +0530

    [ARVIND] : Level-1 -> Added the program file of print circle

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/classandobject/LevelOne/Circle.java
@@ -0,0 +1,11 @@
+package com.classandobject.LevelOne;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintCircle print = new PrintCircle(2.5);
+		print.areaCircle();
+		print.circumferenceCircle();
+	}
+}
---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/classandobject/LevelOne/PrintCircle.java
@@ -0,0 +1,21 @@
+package com.classandobject.LevelOne;
+
+public class PrintCircle {
+	//non-static variables
+		double radius;
+		
+		//constructors
+		PrintCircle(double radius){
+			this.radius=radius;
+		}
+		
+		//Creating method for calculating area of circle
+		public void areaCircle() {
+			System.out.printf("Area of circle: %.4f",(Math.PI*radius*radius));
+		}
+		
+		//Creating method for calculating circumference of circle
+		public void circumferenceCircle() {
+			System.out.printf("\nCircumference of circle: %.4f ",(2*Math.PI*radius));
+		}
+}
