034a71239e29a5039ebef545f082c6c4fb0aa370
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-04 03:12:53 +0530

    [ARVIND] : encapuslation-> Added the program file of ride system

---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/ridesystem/Mini.java
@@ -0,0 +1,8 @@
+package com.encapsulation.ridesystem;
+
+public class Mini extends Vehicle{
+	
+	public Mini(String vehicleNumber) {
+		super(vehicleNumber, 6, "SUV",20);
+	}
+}
---
+++ java-oops-practice/gcr-code-base/JavaOopsConcepts/src/com/encapsulation/ridesystem/SUV.java
@@ -0,0 +1,8 @@
+package com.encapsulation.ridesystem;
+
+public class SUV extends Vehicle{	
+	
+	public SUV(String vehicleNumber) {
+		super(vehicleNumber,4, "SUV",10);
+	}
+}
