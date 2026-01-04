6e32a78a33d85aebbb5a90ba8f53acb258ccf349
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-05 02:33:53 +0530

    [ARVIND] : linkedlist-> Added the program file of undo redo system

--- dsa-practice/gcr-code-base/DataStructure/src/com/linkedlist/undoredo/Main.java
+++ dsa-practice/gcr-code-base/DataStructure/src/com/linkedlist/undoredo/Main.java
@@ -12,10 +12,7 @@ public class Main {
 		t.undo();
 		t.currentText();
 		
-<<<<<<< HEAD
 		t.redo(); 
-=======
->>>>>>> c0eac75b1c5abb34fd9c28cc9c74c50b98e52cbf
 		t.redo();
 		t.currentText();
 		
