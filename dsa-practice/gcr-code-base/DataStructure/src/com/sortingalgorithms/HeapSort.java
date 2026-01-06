f6a47b80cb143b5fef3315999b5732b9716ca650
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-07 03:04:28 +0530

    [ARVIND] : sorting-> Added the program file of heap sort

---
+++ dsa-practice/gcr-code-base/DataStructure/src/com/sortingalgorithms/HeapSort.java
@@ -0,0 +1,19 @@
+package com.sortingalgorithms;
+
+public class HeapSort {
+
+	public static void main(String[] args) {
+int arr[] = {33,223,4,32,23,4,32,2,4,32,3422,2};
+		
+		int n = arr.length;
+		heapSort(arr,0,n-1);
+		for(int i:arr) {
+			System.out.print(i + " ");
+		}
+	}
+	
+	//here the logic of heap Sort
+	public static void heapSort(int arr[], int i, int j) {
+		
+	}
+}
