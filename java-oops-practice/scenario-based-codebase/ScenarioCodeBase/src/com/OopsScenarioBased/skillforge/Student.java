fa07293b0c465a06f33897c306b565d702fcbac3
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-10 14:59:37 +0530

    [ARVIND] : scenario-> Added the program file of skills forge system

---
+++ java-oops-practice/scenario-based-codebase/ScenarioCodeBase/src/com/OopsScenarioBased/skillforge/Student.java
@@ -0,0 +1,21 @@
+package com.OopsScenarioBased.skillforge;
+
+import java.util.*;
+
+public class Student {
+	
+	private String userName;
+	private int courseId;
+	
+	static Map<Integer,ArrayList<Course>> studentEnroll = new HashMap<>();
+	
+	public Student(String userName, int courseId) {
+		this.userName = userName;
+		this.courseId = courseId;
+	}
+	
+	//enroll in course
+	public void enrollStudent(int courseId) {
+		
+	}
+}
