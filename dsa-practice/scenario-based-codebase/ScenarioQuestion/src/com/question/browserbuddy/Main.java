9b32c04b7f20de15bc54ae9d5cb55819ba47af17
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-11 20:25:02 +0530

    [ARVIND] : scenario-> Added the program file of browser tab system

--- dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/browserbuddy/Main.java
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/browserbuddy/Main.java
@@ -3,6 +3,25 @@ package com.question.browserbuddy;
 public class Main {
 
 	public static void main(String[] args) {
+		DoubleLinkedList node1 = new DoubleLinkedList();
+		node1.addTab(new Tab("hello"));
+		node1.addTab(new Tab("how"));
+		node1.addTab(new Tab("are you?"));
 		
+		node1.viewTabs();
+		
+		node1.backwardTab();
+		node1.backwardTab();
+		node1.backwardTab();
+		
+		node1.viewTabs();
+		
+		node1.closeTabs();
+		
+		node1.viewTabs();
+		
+		node1.reopenTabs();
+		
+		node1.viewTabs();
 	}
 }
