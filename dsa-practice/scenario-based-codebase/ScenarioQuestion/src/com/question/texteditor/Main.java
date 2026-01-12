27e518304858d79e2462643c46cce7c4b2b53385
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-12 23:22:43 +0530

    [ARVIND] : scenario-> Added the program file of text editor system

--- dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/texteditor/Main.java
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/texteditor/Main.java
@@ -3,6 +3,23 @@ package com.question.texteditor;
 public class Main {
 
 	public static void main(String[] args) {
+		Text text = new Text();
 		
+		text.insertText("Hii");
+		text.insertText("hello");
+		text.insertText("how");
+		text.insertText("are");
+		text.insertText("you?");
+		
+		
+		text.deleteText();
+		
+		text.undo();
+		
+		text.redo();
+		
+		text.undo();
+		
+		text.viewText();
 	}
 }
---
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/texteditor/Text.java
@@ -0,0 +1,50 @@
+package com.question.texteditor;
+import java.util.Stack;
+
+public class Text {
+	static Stack<String> data = new Stack<>();
+	static Stack<String> restoreData = new Stack<>();
+	
+	//insert text
+	public void insertText(String text) {
+		data.push(text);
+		System.out.println("text added");
+	}
+	
+	//delete text
+	public void deleteText() {
+		if(!data.isEmpty()) {
+			restoreData.push(data.pop());
+			System.out.println("text deleted.");
+		}else {
+			System.out.println("text empty!");
+		}
+	}
+	
+	//undo operation
+	public void undo() {
+		if(!restoreData.isEmpty()) {
+			data.push(restoreData.pop());
+			System.out.println("text undo done.");
+		}else {
+			System.out.println("undo not possible, text empty!");
+		}
+	}
+	
+	//redo
+	public void redo() {
+		if(!data.isEmpty()) {
+			restoreData.push(data.pop());
+			System.out.println("text redo done.");
+		}else {
+			System.out.println("redo not possible, text empty!");
+		}
+	}
+	
+	//view text
+	public void viewText() {
+		while(!data.isEmpty()) {
+			System.out.print(data.pop()+" ");
+		}
+	}
+}
