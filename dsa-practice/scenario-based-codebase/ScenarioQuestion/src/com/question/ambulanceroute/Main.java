ced206c3b504c9e9ec07092d5a2696a11f647eab
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-12 14:52:10 +0530

    [ARVIND] : scenario -> Added the program file of ambulance system

--- dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/ambulanceroute/Main.java
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/ambulanceroute/Main.java
@@ -3,6 +3,16 @@ package com.question.ambulanceroute;
 public class Main {
 
 	public static void main(String[] args) {
+		HospitalManagement mangement = new HospitalManagement();
+		mangement.addHospital("Emergency", true);
+		mangement.addHospital("Radiology", true);
+		mangement.addHospital("Surgery", true);
+		mangement.addHospital("ICU", true);
 		
+		mangement.viewHospital();
+		mangement.underMaintance("Surgery");
+		mangement.viewHospital();
+		
+		mangement.admitPatient("ravi");
 	}
 }
