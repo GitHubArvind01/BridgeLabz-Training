4d029b72628735aa82bb36ede37fd6ac271de199
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-12 17:14:11 +0530

    [ARVIND] : scenario-> Added the program file of train compantment system

---
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/traincompanion/TrainManagement.java
@@ -0,0 +1,57 @@
+package com.question.traincompanion;
+
+public class TrainManagement {
+	Train head;
+	Train curr;
+	//add compartment 
+	public void addCompartment(Train newTrain) {
+		if(head==null) {
+			head = curr  = newTrain;
+			System.out.println("Add compartment node first");
+			return;
+		}
+		
+		curr.next = newTrain;
+		newTrain.prev = curr;
+		curr = newTrain;
+	}
+	
+	//view compartment
+	public void viewCompartment() {
+		int i = 1;
+		Train temp = head;
+		while(temp!=null) {
+			System.out.println("\n-------------Train Compartment "+(i)+"-------------------");
+			System.out.println(temp.compartmentName);
+			System.out.println(temp.compartmentNumber);
+			System.out.println("---------------------------------------------------");
+			i+=1;
+			temp = temp.next;
+		}
+	}
+	
+	//search for pentry
+	public void searchPreviousePentry() {
+		Train temp = curr;
+		while(temp!=head) {
+			if(temp.compartmentName.equalsIgnoreCase("pentry")) {
+				System.out.println("\nPentry found in previouse compartment number "+temp.compartmentNumber);
+				return;
+			}
+			temp = temp.prev;
+		}
+		System.out.println("pentry not found in previouse compartment!");
+	}
+	
+	public void searchForwardPentry() {
+		Train temp = curr;
+		while(temp!=null) {
+			if(temp.compartmentName.equalsIgnoreCase("pentry")) {
+				System.out.println("\nPentry found in forward compartment number "+temp.compartmentNumber);
+				return;
+			}
+			temp = temp.next;
+		}
+		System.out.println("pentry not found in forward compartment!");
+	}
+}
