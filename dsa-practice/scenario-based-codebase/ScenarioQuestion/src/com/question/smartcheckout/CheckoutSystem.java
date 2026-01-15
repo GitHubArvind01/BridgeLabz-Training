0bd97bb6d170963664d639c299bd47dab73358cc
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-15 17:40:42 +0530

    [ARVIND] : scenario-> Added the program file of checkout system

--- dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/smartcheckout/CheckoutSystem.java
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/smartcheckout/CheckoutSystem.java
@@ -4,4 +4,56 @@ import java.util.*;
 public class CheckoutSystem {
 	static HashMap<Integer,Item> itemMap = new HashMap<>();
 	
+	//view item
+	public void viewItem() {
+		int i=1;
+		System.out.println("-----------------------------------------Item List----------------------------------------");
+		if(Item.itemList.isEmpty()) {
+			System.out.println("Item list is empty!");
+			System.out.println("-------------------------------------------------------------------------------------------");
+			return;
+		}
+		for(Item item : Item.itemList) {
+			System.out.println("-----------Item No. "+(i)+"----------");
+			System.out.println("Item Id: "+item.itemId);
+			System.out.println("Item Name: "+item.itemName);
+			System.out.println("Item Price: "+item.price);
+			System.out.println("Item Quantity: "+item.quantity);
+			System.out.println("----------------------------");
+			i+=1;
+		}
+		System.out.println("-------------------------------------------------------------------------------------------");
+	}
+	
+	//checkout
+	public void checkout() {
+		if(Item.itemList.isEmpty()) {
+			System.out.println("Please add item for checkout!");
+			return;
+		}
+		System.out.println("-----------------------Checkout Details-----------------------");
+		if(Customer.customerList.isEmpty()) {
+			System.out.println("Customer List empty!");
+			return;
+		}
+		Customer firstCustomer = Customer.customerList.poll();
+		double totalAmount = 0.0;
+		for(Item item : Item.itemList) {
+			totalAmount+= (item.quantity)*item.price;
+		}
+		System.out.println("Customer Name: "+firstCustomer.name);
+		System.out.println("Total Bill Amount: "+(totalAmount));
+		System.out.println("--------------------------------------------------------------");
+		//at last we clear the list of item in  list
+		Item.itemList.clear();
+	}
+	
+	//is customer exist
+	public boolean isCustomer() {
+		if(Customer.customerList.isEmpty()) {
+			System.out.println("Please create customer first!");
+			return false;
+		}
+		return true;
+	}
 }
--- dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/smartcheckout/Item.java
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/smartcheckout/Item.java
@@ -1,15 +1,32 @@
 package com.question.smartcheckout;
+import java.util.*;
 
 public class Item {
+	static int counter = 1001;
+	
+	static Map<Integer,ArrayList<Item>> customerItemList = new HashMap<>();
+	static List<Item> itemList = new ArrayList<>();
+	
 	String itemName;
 	int itemId;
 	double price;
 	int quantity;
 	
-	public Item(String itemName, double price, int quantity,int itemId) {
+	public Item(String itemName, double price, int quantity) {
 		this.itemName = itemName;
 		this.price = price;
 		this.quantity = quantity;
-		this.itemId = itemId;
+		this.itemId = counter++;
 	}
+	
+	//add item
+	public void addItem(Item item) {
+		if(Customer.customerList.isEmpty()) {
+			System.out.println("Please create customer first!");
+			return;
+		}
+		itemList.add(item);
+	}
+	
+	//hit checkout
 }
