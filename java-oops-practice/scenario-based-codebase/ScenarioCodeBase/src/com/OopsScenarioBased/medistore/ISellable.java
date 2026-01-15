72fef093bbf231c3c3696e51b41abc37f770021a
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-15 13:30:16 +0530

    [ARVIND] : scenario-> Added the program file of medical store system

---
+++ java-oops-practice/scenario-based-codebase/ScenarioCodeBase/src/com/OopsScenarioBased/medistore/ISellable.java
@@ -0,0 +1,8 @@
+package com.OopsScenarioBased.medistore;
+import java.time.LocalDate;
+
+public interface ISellable {
+	public boolean checkExpiry(LocalDate date);
+	
+	public void sell(int quantity);
+}
