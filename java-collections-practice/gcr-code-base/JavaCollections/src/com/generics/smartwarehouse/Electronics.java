339607fc1c99ef2f8314a348d70aaa9a6e5b2ffb
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-17 00:28:28 +0530

    [ARVIND] : generic-> Added the program file of smart warehouse system

---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/generics/smartwarehouse/Electronics.java
@@ -0,0 +1,8 @@
+package com.generics.smartwarehouse;
+
+public class Electronics extends WarehouseItem{
+
+	public Electronics(String itemName, double price, int quantity) {
+		super(itemName, price, quantity);
+	}
+}
---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/generics/smartwarehouse/WarehouseItem.java
@@ -0,0 +1,14 @@
+package com.generics.smartwarehouse;
+
+public abstract class WarehouseItem {
+	String itemName;
+	double price;
+	int quantity;
+	
+	public WarehouseItem(String itemName, double price, int quantity) {
+		super();
+		this.itemName = itemName;
+		this.price = price;
+		this.quantity = quantity;
+	}
+}
