7f662e449ced182cbdc2c865c67b81354c07724b
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-17 03:04:00 +0530

    [ARVIND] : collections-> Added the program file of rotate list

---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/collections/RotateList.java
@@ -0,0 +1,18 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotateList {
+	public static void main(String args[]) {
+		List<Integer> list = new ArrayList<>();
+		list.add(4);
+		list.add(3);
+		list.add(2);
+		list.add(4);
+		list.add(2);
+		
+		int by = 2;
+		
+	}
+}
