d93c4b0f8f26286de355898447ce507c42892901
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-17 11:51:00 +0530

    [ARVIND] : scenario-> Added the program file of zip zip mart system

--- dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/zipzipmart/Main.java
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/zipzipmart/Main.java
@@ -5,54 +5,54 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Sale[] sales = {new Sale(LocalDate.of(2022, 3, 12),120000),new Sale(LocalDate.of(2021,7 , 23),120000),new Sale(LocalDate.of(2012, 9, 7),34120000),new Sale(LocalDate.of(2017, 12, 2),120043),new Sale(LocalDate.of(2022, 8, 5),7250000),new Sale(LocalDate.of(2017, 11, 3),1540000)};
-		merge(sales,0,sales.length-1);
+		Sale[] sales = {new Sale(LocalDate.of(2022, 3, 12),12000),new Sale(LocalDate.of(2021,7 , 23),1400),new Sale(LocalDate.of(2012, 9, 7),34100),new Sale(LocalDate.of(2017, 12, 2),130043),new Sale(LocalDate.of(2022, 8, 5),750000),new Sale(LocalDate.of(2017, 11, 3),14000)};
+		
+	
+		Sale reslut[] = merge(sales,0,sales.length-1);
 		
 		//display
-		for(Sale s : sales) {
+		for(Sale s : reslut) {
 			System.out.println("\nTime: "+s.date);
 			System.out.println("Amount: "+s.amount);
 		}
 	}
 	
 	//merge
-	public static void merge(Sale sale[], int l, int r) {
-		if(l<r) {
-			int mid = (l+r)/2;
-			merge(sale,l,mid);
-			merge(sale,mid+1,r);
-			
-			mergeSort(sale,l,r,mid);
+	public static Sale[] merge(Sale sale[], int l, int r) {
+		if(l>=r) {
+			return new Sale[] {sale[r]};
 		}
+		int mid = (l+r)/2;
+		Sale left[] = merge(sale,l,mid);
+		Sale right[] = merge(sale,mid+1,r);
+		
+		return mergeSort(left,right);
 	}
 	
 	//merge array
-	public static void mergeSort(Sale sale[], int l, int r, int mid) {
-		int n1 = mid-l+1;
-		int n2 = r-mid;
+	public static Sale[] mergeSort(Sale left[],Sale right[]) {
+		int n1 = left.length;
+		int n2 = right.length;
 		
-		Sale left[] = new Sale[n1];
-		Sale right[] = new Sale[n2];
-		
-		System.arraycopy(sale, l, left, 0, n1);
-		System.arraycopy(sale, mid, right, 0, n2);
-		
-		int i=0,j=0,k=l;
+		Sale result[] = new Sale[n1+n2];
+
+		int i=0,j=0,k=0;
 		
 		while(i<n1 && j<n2) {
-			if(left[i].amount<right[j].amount) {
-				sale[k++].amount = left[i++].amount;
+			if(left[i].amount<=right[j].amount) {
+				result[k++] = left[i++];
 			}else {
-				sale[k++].amount = right[j++].amount;
+				result[k++] = right[j++];
 			}
 		}
 		
 		while(i<n1) {
-			sale[k++].amount = left[i++].amount;
+			result[k++] = left[i++];
 		}
 		
 		while(j<n2) {
-			sale[k++].amount = right[j++].amount;
+			result[k++] = right[j++];
 		}
+		return result;
 	}
 }
