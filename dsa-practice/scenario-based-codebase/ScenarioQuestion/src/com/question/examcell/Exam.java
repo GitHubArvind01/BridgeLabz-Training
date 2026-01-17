27f37cae58f809abf0fdbe7a53bc8517b5922524
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-17 13:21:34 +0530

    [ARVIND] : scenario-> Added the program file of student rank sort system

---
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/examcell/Exam.java
@@ -0,0 +1,13 @@
+package com.question.examcell;
+
+public class Exam {
+	String studentName;
+	int rank;
+	double score;
+	
+	public Exam(String studentName, int rank, double score) {
+		this.studentName = studentName;
+		this.rank = rank;
+		this.score = score;
+	}
+}
---
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/examcell/Main.java
@@ -0,0 +1,52 @@
+package com.question.examcell;
+
+public class Main {
+	public static void main(String args[]) {
+		Exam exam[] = {new Exam("Allu",1,355), new Exam("Ravi",4,135), new Exam("kali",56,411), new Exam("suriya",14,233), new Exam("sonu",41,455)};
+		
+		Exam result[] = merge(exam,0,exam.length-1);
+		
+		System.out.println("Rank\t\tName\t\tScore");
+		for(Exam exams : result) {
+			System.out.print(exams.rank+"\t\t"+(exams.studentName)+"\t\t"+(exams.score));
+			System.out.println();
+		}
+	}
+	
+	//merge sort
+	public static Exam[] merge(Exam exam[], int l, int r) {
+		if(l>=r) {
+			return new Exam[]{exam[r]};
+		}
+		int mid = (l+r)/2;
+		
+		Exam left[] = merge(exam,l,mid);
+		Exam right[] = merge(exam,mid+1,r);
+		
+		return mergeSort(left,right);
+	}
+	
+	public static Exam[] mergeSort(Exam left[], Exam right[]) {
+		Exam result[] = new Exam[left.length+right.length];
+		
+		int i=0, j=0,k=0;
+		
+		while(i<left.length && j<right.length) {
+			if(left[i].rank<right[j].rank) {
+				result[k++] = left[i++];
+			}else {
+				result[k++] = right[j++];
+			}
+		}
+		
+		while(i<left.length) {
+			result[k++] = left[i++];
+		}
+		
+		while(j<right.length) {
+			result[k++] = right[j++];
+		}
+		
+		return result;
+	}
+}
