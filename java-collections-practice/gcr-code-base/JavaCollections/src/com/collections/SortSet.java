904bae160250ff208434f063b71a94bb4e724f07
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-18 01:25:17 +0530

    [ARVIND] : collections -> Added the program file of sort set

---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/collections/SortSet.java
@@ -0,0 +1,14 @@
+package com.collections;
+import java.util.*;
+
+public class SortSet {
+
+	public static void main(String[] args) {
+		HashSet<Integer> set1 = new HashSet<>();
+		set1.add(1);
+		set1.add(3);
+		set1.add(4);
+		
+		List<HashSet<Integer>> list = Arrays.asList(set1);
+	}
+}
