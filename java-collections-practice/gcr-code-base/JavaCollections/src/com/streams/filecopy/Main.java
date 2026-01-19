42be29094ec729d898c42b4acddb6e617f2a63fd
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-20 00:26:32 +0530

    [ARVIND] : streams-> Added the program

---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/streams/filecopy/Main.java
@@ -0,0 +1,8 @@
+package com.streams.filecopy;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/streams/readwrite/Main.java
@@ -0,0 +1,8 @@
+package com.streams.readwrite;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
