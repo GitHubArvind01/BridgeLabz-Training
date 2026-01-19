813b45d9e0796fc4b957a05e7993bce9fab9cf5a
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-20 00:33:31 +0530

    [ARVIND] : collections-> Added the program file of sort list

--- java-collections-practice/gcr-code-base/JavaCollections/src/com/collections/SortSet.java
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/collections/SortSet.java
@@ -5,10 +5,24 @@ public class SortSet {
 
 	public static void main(String[] args) {
 		HashSet<Integer> set1 = new HashSet<>();
+		set1.add(13);
 		set1.add(1);
-		set1.add(3);
+		set1.add(0);
 		set1.add(4);
+		set1.add(5);
+		set1.add(4);
+		set1.add(9);
+
 		
-		List<HashSet<Integer>> list = Arrays.asList(set1);
+		ArrayList<Integer> list = new ArrayList<>(set1);
+		Collections.sort(list);
+		
+		set1.clear();
+		
+		HashSet<Integer> set2 = new HashSet<>(list);
+		
+		for(int num : set2) {
+			System.out.print(num+ " ");
+		}
 	}
 }
