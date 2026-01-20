daf2e167dc5eadce51ddd6346c655e772773439e
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-20 16:59:38 +0530

    [ARVIND] : scenario-> Added the program file of fitness tracker

---
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/fitnesstracker/Track.java
@@ -0,0 +1,11 @@
+package com.question.fitnesstracker;
+
+public class Track {
+	String name;
+	int steps;
+	
+	public Track(String name, int steps) {
+		this.name = name;
+		this.steps = steps;
+	}
+}
