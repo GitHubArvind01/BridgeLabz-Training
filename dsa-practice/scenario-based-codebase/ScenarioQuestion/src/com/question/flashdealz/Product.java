5f1270b7ff07e3e7bf8d7c27982e2896e9b3621d
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-20 13:33:00 +0530

    [ARVIND] : scenario-> Added the program file of flash deal system

---
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/flashdealz/Product.java
@@ -0,0 +1,11 @@
+package com.question.flashdealz;
+
+public class Product {
+	String name;
+	double percentage;
+	
+	public Product(String name, double percentage) {
+		this.name = name;
+		this.percentage = percentage;
+	}
+}
