bdc51cf2d23725f0b73d6e9ccf9d818788a9a69b
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-21 12:04:17 +0530

    [ARVIND] : scenario-> Added the program file of food nest footfall most system

---
+++ dsa-practice/scenario-based-codebase/ScenarioQuestion/src/com/question/foodfest/Stall.java
@@ -0,0 +1,11 @@
+package com.question.foodfest;
+
+public class Stall {
+	String stallName;
+	int countSold;
+	
+	public Stall(String stallName, int countSold) {
+		this.stallName = stallName;
+		this.countSold = countSold;
+	}
+}
