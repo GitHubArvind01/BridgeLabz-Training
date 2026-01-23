cc312b0e1af379aabf71eac7ca0d00d6a83df941
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-23 23:38:13 +0530

    [ARVIND] : annotation-> Added the program file of annotation and relfections

---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/annotationsreflection/accessprivatefield/Main.java
@@ -0,0 +1,8 @@
+package com.annotationsreflection.accessprivatefield;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/annotationsreflection/classinformation/Main.java
@@ -0,0 +1,8 @@
+package com.annotationsreflection.classinformation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
