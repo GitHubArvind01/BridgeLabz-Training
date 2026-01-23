6d96c21565423cb3147fcbd9b663c508b4f3f1e5
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-24 00:30:13 +0530

    [ARVIND] : scenario-> Added the intial setup

---
+++ java-collections-practice/scenario-based-codebase/CollectionScenarios/src/com/questions/resumeanalyzer/Main.java
@@ -0,0 +1,8 @@
+package com.questions.resumeanalyzer;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ java-collections-practice/scenario-based-codebase/CollectionScenarios/src/com/questions/travellog/Main.java
@@ -0,0 +1,8 @@
+package com.questions.travellog;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
