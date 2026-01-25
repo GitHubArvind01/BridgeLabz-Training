99709e23b6292ca38653529ac4319bbda46831c4
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-25 19:25:27 +0530

    [ARVIND] : junit-> Added the program file of

---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/regexjunit/junitquestions/testingcalculator/Main.java
@@ -0,0 +1,8 @@
+package com.regexjunit.junitquestions.testingcalculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/regexjunit/junitquestions/testingstringutility/Main.java
@@ -0,0 +1,8 @@
+package com.regexjunit.junitquestions.testingstringutility;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
