f28c8f8de8c2d1f74a199ff33a4748b4e6b6d3ed
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-26 23:56:41 +0530

    [ARVIND] :  csv-> Added the program file of read csv file

--- java-iostreams-practice/gcr-code-base/IOProgamming/src/com/csv_data_handling/readcsv/Main.java
+++ java-iostreams-practice/gcr-code-base/IOProgamming/src/com/csv_data_handling/readcsv/Main.java
@@ -1,8 +1,26 @@
 package com.csv_data_handling.readcsv;
 
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("DataFiles/student.csv")));
+			String line = reader.readLine();
+			while((line=reader.readLine())!=null) {
+				String data[] = line.split(",");
+				System.out.println("-----------------------");
+				System.out.println("Id: "+data[0]);
+				System.out.println("Name: "+data[1]);
+				System.out.println("Age: "+data[2]);
+				System.out.println("Score: "+data[3]);
+				System.out.println("-----------------------");
+			}
+			
+		}
+		catch(IOException e) {
+			System.out.println(""+e.getMessage());
+		}
 	}
 }
