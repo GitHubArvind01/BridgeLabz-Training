217346386b183e7ec18f3b92734a8850e21b8c53
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-26 18:11:19 +0530

    [ARVIND] : annotation-> Added the program file of multiple method annotation length

---
+++ java-collections-practice/gcr-code-base/JavaCollections/src/com/annotationsreflection/annotations/annotationmaxlength/Main.java
@@ -0,0 +1,8 @@
+package com.annotationsreflection.annotations.annotationmaxlength;
+
+public class Main {
+	 public static void main(String[] args) {
+        new User("Arvind");          
+        new User("VeryLongUsername");
+    }
+}
