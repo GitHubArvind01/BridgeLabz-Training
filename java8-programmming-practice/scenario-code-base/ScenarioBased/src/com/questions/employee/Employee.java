8a498b29ce0022e3485ebba1e8fbc84288351fd6
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-28 17:09:47 +0530

    [ARVIND] : scenario-> Added the program file of employee using java8 stream

--- java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/employee/Employee.java
+++ java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/employee/Employee.java
@@ -6,11 +6,10 @@ public class Employee {
 	private int age;
 	private String gender;
 	private String department;
-	private double yearOfJoining;
+	private int yearOfJoining;
 	private double salary;
 	
-	public Employee(int id, String name, int age, String gender, String department, double yearOfJoining,
-			double salary) {
+	public Employee(int id, String name, int age, String gender, String department, int yearOfJoining, double salary) {
 		this.id = id;
 		this.name = name;
 		this.age = age;
@@ -19,63 +18,49 @@ public class Employee {
 		this.yearOfJoining = yearOfJoining;
 		this.salary = salary;
 	}
-
+	
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 	public String getDepartment() {
 		return department;
 	}
-
 	public void setDepartment(String department) {
 		this.department = department;
 	}
-
-	public double getYearOfJoining() {
+	public int getYearOfJoining() {
 		return yearOfJoining;
 	}
-
-	public void setYearOfJoining(double yearOfJoining) {
+	public void setYearOfJoining(int yearOfJoining) {
 		this.yearOfJoining = yearOfJoining;
 	}
-
 	public double getSalary() {
 		return salary;
 	}
-
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", age=" + age + ", gender=" + gender + ", department="
--- java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/employee/EmployeeMain.java
+++ java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/employee/EmployeeMain.java
@@ -1,17 +1,18 @@
 package com.questions.employee;
 
+import java.util.Comparator;
 import java.util.stream.Collectors;
 
 public class EmployeeMain {
 
 	public static void main(String[] args) {
 		
-		Employee ramu = new Employee(101,"ramu",21,"male","IT",5.5,980000);
-		Employee sujata = new Employee(102,"sujta",31,"female","IT",2,70000);
-		Employee kali = new Employee(103,"kali",37,"male","Automation",5.5,1390000);
-		Employee ravi = new Employee(104,"ravi",45,"male","Pharma",5.5,890000);
-		Employee suriya = new Employee(105,"suriya",32,"male","Hardware",5.5,90000);
-		Employee soniya = new Employee(106,"soniya",55,"female","HR",5,30000);
+		Employee ramu = new Employee(101,"ramu",21,"male","IT",2015,980000);
+		Employee sujata = new Employee(102,"sujta",31,"female","IT",2022,70000);
+		Employee kali = new Employee(103,"kali",37,"male","Automation",2014,1390000);
+		Employee ravi = new Employee(104,"ravi",45,"male","Pharma",2011,890000);
+		Employee suriya = new Employee(105,"suriya",32,"male","Hardware",2000,90000);
+		Employee soniya = new Employee(106,"soniya",55,"female","HR",2026,30000);
 		
 		EmployeeDataCenter data = new EmployeeDataCenter();
 		data.add(ramu);
@@ -42,6 +43,34 @@ public class EmployeeMain {
 		
 		//3. What is the average age of male and female employees ?
 		System.out.println("\n3. What is the average age of male and female employees ?");
-		System.out.println(		data.employeeList.stream().collect(Collectors.groupingBy(Employee::getGender,Collectors.averagingInt(Employee::getAge))));
+		System.out.println(data.employeeList.stream()
+											.collect(Collectors.groupingBy(Employee::getGender,Collectors
+											.averagingInt(Employee::getAge)
+														)
+													)
+												);
+		
+		//4. Get the details of highest paid employee in the organization ?
+		System.out.println("\n4. Get the details of highest paid employee in the organization ?");
+		System.out.println(
+					data.employeeList.stream()
+									.collect(Collectors
+											.maxBy(Comparator
+													.comparing(Employee::getSalary)))
+				);
+		
+		
+		//5. Get the names of all employees who have joined after 2015 ?
+		System.out.println("\n 5. Get the names of all employees who have joined after 2015 ?");
+		data.employeeList.stream()
+							.filter(Employee-> Employee.getYearOfJoining()>=2015)
+							.forEach(System.out::println);
+		
+		//6. Count the number of employees in each department ?
+		System.out.println("\n6. Count the number of employees in each department ?");
+		System.out.println(data.employeeList.stream()
+											.collect(Collectors.groupingBy(Employee::getDepartment,Collectors.counting())));
+		
+		
 	}
 }
