30586717496282c09a628562250e6e9a01b355bf
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-29 01:09:16 +0530

    [ARVIND] : scenario-> Added the progam file of student system in java8 to perform stram API

--- java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/student/StudentMain.java
+++ java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/student/StudentMain.java
@@ -3,5 +3,14 @@ package com.questions.student;
 public class StudentMain {
 
 	public static void main(String[] args) {
+		Student ravi = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+		Student sonu = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+		Student kavi = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+		Student somya = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+		Student kali = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+		Student suriya = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+		Student yuk = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+		Student alice = new Student("Allu Arvind",101,"Allu",21,"male","bhopal",220,"748737373");
+
 	}
 }
