37691211df46cbee97fcea24c2d0b8fec5d8b5ff
GitHubArvind01 <kumararvind21390@gmail.com>
2026-01-30 13:04:37 +0530

    [ARVIND] : scenario-> Added the program file of student system with java 8 stream API

--- java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/student/Student.java
+++ java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/student/Student.java
@@ -10,9 +10,9 @@ public class Student {
 	private String city;
 	private double rank;
 	private String contacts;
-	
+	private String department;
 	public Student(String name, int id, String firstName, int age, String gender, String city, double rank,
-			String contacts) {
+			String contacts, String department) {
 		Name = name;
 		this.id = id;
 		this.firstName = firstName;
@@ -21,75 +21,66 @@ public class Student {
 		this.city = city;
 		this.rank = rank;
 		this.contacts = contacts;
+		this.department = department;
 	}
-
 	public String getName() {
 		return Name;
 	}
-
 	public void setName(String name) {
 		Name = name;
 	}
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getFirstName() {
 		return firstName;
 	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-
 	public int getAge() {
 		return age;
 	}
-
 	public void setAge(int age) {
 		this.age = age;
 	}
-
 	public String getGender() {
 		return gender;
 	}
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
 	public String getCity() {
 		return city;
 	}
-
 	public void setCity(String city) {
 		this.city = city;
 	}
-
 	public double getRank() {
 		return rank;
 	}
-
 	public void setRank(double rank) {
 		this.rank = rank;
 	}
-
 	public String getContacts() {
 		return contacts;
 	}
-
 	public void setContacts(String contacts) {
 		this.contacts = contacts;
 	}
-
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
 	@Override
 	public String toString() {
 		return "Student [Name=" + Name + ", id=" + id + ", firstName=" + firstName + ", age=" + age + ", gender="
-				+ gender + ", city=" + city + ", rank=" + rank + ", contacts=" + contacts + "]";
+				+ gender + ", city=" + city + ", rank=" + rank + ", contacts=" + contacts + ", department=" + department
+				+ "]";
 	}
 }
