02cbe4fa4b30e7b3d0f82b40fdb5f257b727dc0d
GitHubArvind01 <kumararvind21390@gmail.com>
2026-02-05 02:29:08 +0530

    [ARVIND] : scenario-> Added the program file of take one input string and perform many actions through stream API

---
+++ java8-programmming-practice/scenario-code-base/ScenarioBased/src/com/questions/stream_with_string/Main.java
@@ -0,0 +1,32 @@
+package com.questions.stream_with_string;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*
+		 	1.Character Frequency Count
+			
+			Input: "banana"
+			Output: {b=1, a=3, n=2}
+		 */
+		
+		System.out.println("1.Character Frequency Count");
+		String input1 = "banana";
+		Map<Character, Long> charfreq = input1.chars()
+												.mapToObj(c-> (char) c)
+												.filter(c-> (c!=' '))
+												.collect(Collectors.groupingBy(
+															Function.identity(),
+															LinkedHashMap::new,
+															Collectors.counting()
+														));
+		
+		System.out.println(charfreq);
+	}
+}
