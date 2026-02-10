b7bf09498e142243c98a677017928191beeb48a2
GitHubArvind01 <kumararvind21390@gmail.com>
2026-02-11 00:59:31 +0530

    [ARVIND] : Added the program file of jdbc setup

--- mysql-jdbc-practice/gcr-code-base/jdbc/src/main/java/com/practices/jdbc/practices/jdbc_setup/Main.java
+++ mysql-jdbc-practice/gcr-code-base/jdbc/src/main/java/com/practices/jdbc/practices/jdbc_setup/Main.java
@@ -1,8 +1,37 @@
 package com.practices.jdbc.practices.jdbc_setup;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
 
+	// Database credentials
+	private static final String URL = "jdbc:mysql://localhost:3306/jdbc_demo";
+	private static final String USER = "root";
+	private static final String PASSWORD = "alluMysql@071";
+	
+	public static Connection getConnection() {
+		Connection connection = null;
+		try {
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println("Database Connected Success");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	
 	public static void main(String[] args) {
-		
+		Connection connection = getConnection();
+		if(connection==null) {
+			try {
+				connection.close();
+			}
+			catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
