b724e552e8a579e7db1c3bf77f13e614960e71b9
GitHubArvind01 <kumararvind21390@gmail.com>
2026-02-10 23:58:19 +0530

    [ARVIND] : Added the practices problem set on jdbc

---
+++ mysql-jdbc-practice/gcr-code-base/jdbc/src/main/java/com/practices/jdbc/practices/jdbc_setup/Main.java
@@ -0,0 +1,8 @@
+package com.practices.jdbc.practices.jdbc_setup;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ mysql-jdbc-practice/gcr-code-base/jdbc/src/main/java/com/practices/jdbc/practices/jdbc_setup2/Main.java
@@ -0,0 +1,8 @@
+package com.practices.jdbc.practices.jdbc_setup2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+}
